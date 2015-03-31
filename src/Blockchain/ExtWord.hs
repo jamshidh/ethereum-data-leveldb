8797bfffd3f936bb08effcfe8bfffe2218e7a953
jim <jim>
2015-03-31 12:58:56 -0700

    Adding type Word128.

--- src/Blockchain/ExtWord.hs
+++ src/Blockchain/ExtWord.hs
@@ -2,16 +2,19 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Blockchain.ExtWord (
+  Word128, 
   Word160,
   Word256,
   Word512,
+  word128ToBytes,
+  bytesToWord128,
   word256ToBytes,
   bytesToWord256
   ) where
 
 import Data.Bits
 import Data.Word
-import Network.Haskoin.Internals (Word160, Word256, Word512)
+import Network.Haskoin.Internals (Word128, Word160, Word256, Word512)
 
 import Data.Ix
 
@@ -23,6 +26,14 @@ instance Ix Word256 where
     inRange (x, y) z | z >= x && z <= y = True 
     inRange _ _ = False
 
+word128ToBytes::Word128->[Word8]
+word128ToBytes word = map (fromIntegral . (word `shiftR`)) [128-8, 128-16..0]
+  
+bytesToWord128::[Word8]->Word128
+bytesToWord128 bytes | length bytes == 16 =
+  sum $ map (\(shiftBits, byte) -> fromIntegral byte `shiftL` shiftBits) $ zip [128-8,128-16..0] bytes
+bytesToWord128 _ = error "bytesToWord128 was called with the wrong number of bytes"  
+
 word256ToBytes::Word256->[Word8]
 word256ToBytes word = map (fromIntegral . (word `shiftR`)) [256-8, 256-16..0]
   
@@ -30,3 +41,4 @@ bytesToWord256::[Word8]->Word256
 bytesToWord256 bytes | length bytes == 32 =
   sum $ map (\(shiftBits, byte) -> fromIntegral byte `shiftL` shiftBits) $ zip [256-8,256-16..0] bytes
 bytesToWord256 _ = error "bytesToWord256 was called with the wrong number of bytes"  
+
